422e4fbc7384e0dc2e780854e9f2a55de137f5fc
Rafaelmaiden <rafaelmaggot64@gmail.com>
2021-08-04 11:37:37 -0300

    adjusts in layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,21 @@
 <template>
   <b-container fluid>
     <b-row
+      class="m-0 row-header"
       align-h="between"
-      no-gutters
-      style="padding: 18px 114px; max-width: 100%; --bs-gutter-x: 0rem;" class=" ">
-      <b-col md="6" sm="12" class="p-0 center-contact-logo">
+      style="padding: 18px 114px; max-width: 100%; --bs-gutter-x: 0rem;">
+      <b-col md="6" sm="12" class="p-0 m-0 center-contact-logo">
         <img src="../assets/img/header/logo.png" alt="Logo">
       </b-col>
 
-      <b-col md="6" sm="12" style="text-align: right" class="p-0 center-contact-logo">
+      <b-col md="6" sm="12" style="text-align: right" class="p-0 m-0 center-contact-logo">
         <span class="call-center"> CENTRAL DE ATENDIMENTO</span> <br>
         <span class="phone-number">(31) 3123-1234</span>
       </b-col>
     </b-row>
 
-    <b-row>
-      <b-col>
+    <b-row class="p-0 m-0">
+      <b-col class="p-0 m-0">
         <b-img fluid :src="require('../assets/img/header/retangulo1.png')" alt="Img Civic" class="background-civic"> </b-img>
         <div class="civic-10">
           <b-img fluid :src="require('../assets/img/header/civic_solo.png')" alt="Img Civic"> </b-img>
@@ -52,8 +52,8 @@ export default {
   color: #DF0209;
 }
 
-.col, .row {
-  padding: 0 !important;
-  margin: 0 !important;
+.col{
+  padding: 0;
+  margin: 0;
 }
 </style>"
